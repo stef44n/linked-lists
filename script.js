3ce6f9cbd126c495dae83d5fa80c416e80013b65
Stefan <stefanseifeddine@gmail.com>
2022-10-28 20:26:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const linkedList = () => {};
+
+const Node = () => {
+    function value() {
+        let defaultValue = null;
+    }
+    function nextNode() {
+        let defaultNextNode = null;
+    }
+};
